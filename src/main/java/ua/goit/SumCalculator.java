3490606f0c6b3dc7e44f886bea34235da055b6c8
Admin <mondorin629@gmail.com>
2024-08-15 16:05:31 +0300

    Added class SumCalculator and test SumCalculatorTest

---
+++ src/main/java/ua/goit/SumCalculator.java
@@ -0,0 +1,11 @@
+package ua.goit;
+
+public class SumCalculator {
+
+    public int sum(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("n must be greater than 0");
+        }
+        return n * (n + 1) / 2;
+    }
+}
